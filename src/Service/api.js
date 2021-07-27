ca2fbcd455d0794817f03c9e7660c283e7853d20
[firstname lastname] <helloworldprogrammer612@gmail.com>
2021-07-27 13:15:32 +0530

    Save Changes-1

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "dd2b3781e1370c2b4d22ccbc9474c880";
-const URL = "http://api.openweathermap.org/data/2.5/weather";
+const URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeather = async (city, country) => {
   const data = await axios.get(
